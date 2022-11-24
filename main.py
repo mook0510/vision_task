95a2690f50404871d9c817e9a980dedc0af74eb9
mook0510 <80958575+mook0510@users.noreply.github.com>
2022-11-24 20:44:56 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from cv2 import COLOR_BGR2GRAY
 
 # import numpy as np
 # from PIL import ImageFilter
+# from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
 
 
 class MainWindow(QMainWindow):
@@ -21,10 +22,12 @@ class MainWindow(QMainWindow):
         self.menu_file = self.menu.addMenu("파일")
         exit = QAction("나가기", self, triggered=qApp.quit)
         self.menu_file.addAction(exit)
+        self.tmp_image = None
 
-        save = QAction("저장", self)
-        self.menu_file.addAction(save)
-        
+
+        # save = QAction("저장", self) cv2.imwrite (img_file, )
+        # self.menu_file.addAction(save)
+
         # 메인화면 레이아웃
         main_layout = QHBoxLayout()
 
@@ -64,6 +67,18 @@ class MainWindow(QMainWindow):
         button9.clicked.connect(self.dsize)
         sidebar.addWidget(button9)
 
+        button10 = QPushButton("이미지 합치기")
+        button10.clicked.connect(self.add)
+        sidebar.addWidget(button10)
+
+        button11 = QPushButton("이미지 저장")
+        button11.clicked.connect(self.save_file_dialog)
+        sidebar.addWidget(button11)
+
+        button12 = QPushButton("모자이크")
+        button12.clicked.connect(self.mosaic)
+        sidebar.addWidget(button12)
+
         main_layout.addLayout(sidebar)
 
         self.label1 = QLabel(self)
@@ -77,6 +92,16 @@ class MainWindow(QMainWindow):
         widget = QWidget(self)
         widget.setLayout(main_layout)
         self.setCentralWidget(widget)
+
+
+    # def save_file_dialog(self):
+    #     fpath, _ = QFileDialog.getSaveFileName(self, 'Save Image', '', "PNG(*.png);;JPEG(*.jpg *.jpeg);;All Files(*.*) ")
+    #     if fpath:
+    #         self.image.save(fpath)
+
+    def save_file_dialog(self):
+        cv2.imwrite("self.tmp_image")
+
 
 
     def show_file_dialog(self):
@@ -169,7 +194,7 @@ class MainWindow(QMainWindow):
     def flop(self):
         image = cv2.flip(self.image, 0)
         h, w, _ = image.shape
-        print(image.shape)
+        # print(image.shape)
         bytes_per_line = 3 * w
         image = QImage(
             image.data, w, h, bytes_per_line, QImage.Format_RGB888
@@ -179,16 +204,6 @@ class MainWindow(QMainWindow):
 
     # 흑백
     def black(self):
-        # image = cv2.cvtColor(self.image, cv2.COLOR_RGB2GRAY)
-        # print(image.shape)
-        # h, w = image.shape
-        # bytes_per_line = 3 * w
-        # image = QImage(
-        #     image.data, w, h, bytes_per_line, QImage.Format_RGB888
-        # ).rgbSwapped()
-        # pixmap = QPixmap(image)
-        # self.label2.setPixmap(pixmap)
-
         gray_image = self.image.copy()
         image = cv2.cvtColor(gray_image, cv2.COLOR_BGR2GRAY)
         h, w = image.shape
@@ -208,29 +223,78 @@ class MainWindow(QMainWindow):
         # image = cv2.cvtColor(self.image, cv2.COLOR_RGB2GRAY)
         # grayscale
 
-    # 이미지 흑백
-    # def filter(self):
-    #     image = image.filter(ImageFilter.GaussianBlur(10))
+    # 카피떠서 저장
+    # 수정된 이미지 저장
+    # 타 기능들로 원본 사진에 효과를 적용시킨 사진(toolbar를 기준으로 오른쪽에 출력되는 이미지)을 현재 위치에
+    # newimagefile 이란 이름으로 저장하는 기능
 
-    # original = cv2.imread(fname, cv2.IMREAD_COLOR)
-    # gray = cv2.imread(fname, cv2.IMREAD_GRAYSCALE)
-    # unchange = cv2.imread(fname, cv2.IMREAD_UNCHANGED)
 
-    # def filter(self):
-    #     img = cv2.imread("sudoku.jpg")
-    #     img = img[..., ::-1]
+    # 이미지 합치기
+    def add(self):
+        # image = cv2.add(self.image, cv2.IMREAD_COLOR)
+        # image = cv2.imread(self.image, cv2.IMREAD_COLOR)
+        # image2 = cv2.imread("opencv.png", cv2.IMREAD_COLOR)
+        # h, w, _ = image.shape
+        # bytes_per_line = 3 * w
+        # image = QImage(
+        #     image.data, w, h, bytes_per_line, QImage.Format_RGB888
+        # ).rgbSwapped()
+        # pixmap = QPixmap(image)
+        # self.label2.setPixmap(pixmap)
 
-    #     gx_kernel = np.array([-1, 1])
-    #     gy_kernel = np.array([[-1], [1]])
+        image = cv2.imread(self.image, cv2.IMREAD_COLOR)
+        image2 = cv2.imread("opencv.png", cv2.IMREAD_COLOR)
+        image3 = cv2.add(image, image2)
 
-    #     edge_gx = cv2.filter2D(img, -1, gx_kernel)
-    #     edge_gy = cv2.filter2D(img, -1, gy_kernel)
+        h, w, _ = image.shape
+        bytes_per_line = 3 * w
+        image = QImage(
+            image.data, w, h, bytes_per_line, QImage.Format_RGB888
+        ).rgbSwapped()
+        pixmap = QPixmap(image3)
+        self.label2.setPixmap(pixmap)
 
-    # 이미지 저장
-    # def save(self):
-    #     image = cv2.imread('./cat_save.jpg', cv2.IMREAD_GRAYSCALE)
-    #     cv2.imwrite('./cat_save.jpg', image)
+# img_fg = cv2.imread("opencv_logo.png", cv2.IMREAD_UNCHANGED)  # 4채널 RGBA opencv BGRA
+# img_bg = cv2.imread("cat-01.jpg")  # 3채널 RGB -> BGR
+# img_bg = cv2.cvtColor(img_bg, cv2.COLOR_BGR2RGB)
+# fig, axes = plt.subplots(ncols=2)
+# axes[0].imshow(img_fg)
+# axes[1].imshow(img_bg)
+# resized_img_fg = cv2.resize(img_fg, (int(487*0.5), int(600*0.5)))
+# print(f"원본 로고 이미지의 높이: {img_fg.shape[0]} 너비: {img_fg.shape[1]}")
+# print(f"처리된 로고 이미지의 높이: {resized_img_fg.shape[0]} 너비: {resized_img_fg.shape[1]}")
+# _, mask = cv2.threshold(resized_img_fg[:, :, 3], 1, 255, cv2.THRESH_BINARY)
+# mask_inv = cv2.bitwise_not(mask)
+# print(mask.shape)
+# print(mask)
+# print(mask_inv)
+# print(resized_img_fg.shape)
+# resized_img_fg = cv2.cvtColor(resized_img_fg, cv2.COLOR_BGRA2BGR)
+# print(resized_img_fg.shape)
+# h, w, _ = resized_img_fg.shape
+# print(f"높이: {h} 너비: {w}")
+# print(f"배경사진 높이: {img_bg.shape[0]} 너비: {img_bg.shape[1]}")
+# roi = img_bg[10:10+h, 10:10+w]
+# print(f"관심영역 높이: {roi.shape[0]} 너비: {roi.shape[1]}")
+# resized_img_fg = cv2.cvtColor(resized_img_fg, cv2.COLOR_BGR2RGB)
+# masked_fg = cv2.bitwise_and(resized_img_fg, resized_img_fg, mask=mask)
+# masked_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
+# added = masked_fg + masked_bg
+# plt.imshow(added)
 
+
+    def mosaic(self):
+        small = cv2.resize(self.image, None, fx=0.1, fy=0.1, interpolation=cv2.INTER_NEAREST)
+        image =  cv2.resize(small, self.image.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
+        self.tmp_image = image
+
+        h, w, c = image.shape
+        bytes_per_line = 3 * w
+        image = QImage(
+            image.data, w, h, bytes_per_line, QImage.Format_RGB888
+        ).rgbSwapped()
+        pixmap = QPixmap(image)
+        self.label2.setPixmap(pixmap)
 
 
 if __name__ == "__main__":
